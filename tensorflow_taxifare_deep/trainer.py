c76bf6a3a278c903ea9ac5f3d2737bb88f15b97e
Gaetan Manchon <gmanchon@gmail.com>
2022-03-07 12:30:42 +0100

    add trainer

---
+++ tensorflow_taxifare_deep/trainer.py
@@ -0,0 +1,17 @@
+
+class Trainer:
+
+    def __init__(self, nrows):
+        self.nrows = nrows
+
+    def clean(self):
+        pass
+
+    def preproc(self, test_size):
+        pass
+
+    def fit(self, plot_history, verbose):
+        pass
+
+    def evaluate(self):
+        pass
